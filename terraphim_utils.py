baf50d3a1ca7ba0c8e5e3acb1fb0067fc75083d9
AlexMikhalev <alex@metacortex.engineer>
2023-08-11 13:45:28 +0100

    new loadautomata
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- terraphim_utils.py
+++ terraphim_utils.py
@@ -45,7 +45,7 @@ def load_automata(url, lazy=False):
                 A.add_word(row[0].lower(), (id, row[0]))
                 # print(f"add to automata {row[0]} - {id}")
     A.make_automaton()
-    print(A.get_stats())
+    # print(A.get_stats())
     return A
 
 def find_matches(sent_text, A):
@@ -71,18 +71,18 @@ if __name__ == "__main__":
     haystack = "I am a text with the word Organization strategic plan and bar and project calendar"
     print(haystack)
     url = "https://terraphim-automata.s3.eu-west-2.amazonaws.com/project_manager.csv.gz"
-    Automata = load_csv_to_automata(url)
+    Automata = load_automata(url, lazy=True)
     matches = find_matches(haystack.lower(),Automata)
-    print(matches)
-    Automata = load_csv_to_automata(url,lazy=False)
+    print("Matched Lazy Project manager ",matches)
+    Automata = load_automata(url,lazy=False)
     matches = find_matches(haystack.lower(),Automata)
-    print(matches)
+    print("Matched Project manager ",matches)
     print ("=====================")
     print ("Test System Operator role")
     haystack="""
     The role of systems engineering (SE) during the operation of a system consists of ensuring that the system maintains key mission and business functions and is operationally effective. The systems engineer is one of the stakeholders who ensures that maintenance actions and other major changes are performed according to the long-term vision of the system. Both the maintenance actions and any implemented changes must meet the evolving needs of owning and operating stakeholders consistent with the documented and approved architecture. SE considerations will also include the eventual decommissioning or disposal of the system so that the disposal occurs according to disposal/retirement plans. Those plans must account for and be compliant with relevant laws and regulations (for additional information on disposal or retirement, please see the Product and Service Life Management knowledge area (KA)). When the system-of-interest (SoI) replaces an existing or legacy system, it may be necessary to manage the migration between systems such that stakeholders do not experience a breakdown in services (INCOSE 2012).
     """
     url = "https://system-operator.s3.eu-west-2.amazonaws.com/term_to_id.csv.gz"
-    Automata = load_csv_to_automata(url,lazy=False)
+    Automata = load_automata(url,lazy=False)
     matches = find_matches(haystack.lower(),Automata)
-    print(matches)
+    print("Matched System Operator ",matches)
